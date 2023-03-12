ba6d80c46b02f44105448dc018a05a5e36dcfa27
user <yulechkaakopyan@yandex.ru>
2023-03-13 01:02:27 +0300

    Создание теста

---
+++ src/main/java/com/example/testings/TestingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testings/User.java
@@ -0,0 +1,2 @@
+package com.example.testings;public class User {
+}
