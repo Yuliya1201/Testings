722ee698318efed47d1627e255ba43ec93d0cc9b
user <yulechkaakopyan@yandex.ru>
2023-03-21 01:07:15 +0300

    ДЗ

--- src/main/java/com/example/testings/TestingsApplication.java
+++ src/main/java/com/example/testings/TestingsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestingsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestingsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestingsApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/testings/TestingsApplicationTests.java
+++ src/test/java/com/example/testings/TestingsApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.testings;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestingsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    public void testUserConstructor() {
+        final String login = "login";
+        final String email = "email";
+
+        User user = new User(login, email);
+
+        String userLogin = user.getLogin();
+        String userEmail = user.getEmail();
+
+        Assertions Assert = null;
+        Assert.assertEquals(login, userLogin);
+        Assert.assertEquals(email, userEmail);
+    }
 }
+
+
+
